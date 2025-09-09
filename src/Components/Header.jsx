ced24010d7d6b6e1043f749bb097866eaa8a355e
Artemis <artemis.s.wilkinson@gmail.com>
2025-09-09 16:41:35 +0100

    Add header & nav layout with simple styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,9 @@ import { Navbar } from "./Navbar";
 
 export const Header = () => {
   return (
-    <div className="title-and-nav">
-      <h1>Artemis_Portfolio.exe </h1> <Navbar />
+    <div className="head-container">
+      <h1>Artemis_Portfolio.exe </h1>
+      <Navbar />
     </div>
   );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,15 @@
 export const Navbar = () => {
   return (
-    <nav>
-      <h2>Navbar</h2>
+    <nav className="navigation">
+      <button>
+        <a href="https://github.com/artiechokeheart">About Me</a>
+      </button>
+      <button>
+        <a href="">Skills</a>
+      </button>
+      <button>
+        <a href="">Projects</a>
+      </button>
     </nav>
   );
 };
